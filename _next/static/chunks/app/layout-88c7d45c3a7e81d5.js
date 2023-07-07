f29c49d3a9821773fb20fbeadf43ae77312bc17a
Bran <june20516@gmail.com>
2023-07-07 20:11:19 +0900

    [publish]20:11, 07/07/2023

---
+++ _next/static/chunks/app/layout-88c7d45c3a7e81d5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{7561:function(e,n,t){Promise.resolve().then(t.t.bind(t,8920,23)),Promise.resolve().then(t.t.bind(t,8707,23)),Promise.resolve().then(t.t.bind(t,4121,23))},4121:function(){}},function(e){e.O(0,[920,707,667,488,744],function(){return e(e.s=7561)}),_N_E=e.O()}]);
