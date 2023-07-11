c1d83b5adbc0c9cb5702c11e8aa54bc075f1eb4a
Bran <june20516@gmail.com>
2023-07-11 15:00:41 +0900

    [publish]15:00, 11/07/2023

---
+++ _next/static/chunks/app/posts/[slug]/page-b4d7a00917a93a85.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[333],{9394:function(n,e,t){Promise.resolve().then(t.t.bind(t,7064,23)),Promise.resolve().then(t.t.bind(t,2075,23))},7064:function(){},2075:function(){}},function(n){n.O(0,[667,488,744],function(){return n(n.s=9394)}),_N_E=n.O()}]);
